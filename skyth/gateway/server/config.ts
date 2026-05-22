fe104beea7d91fd85e7ea304219909460765449b
skyth-engine <skyth-engine@proton.me>
2026-05-22 15:17:00 +0000

    Move default Skyth gateway port

--- skyth/gateway/server/config.ts
+++ skyth/gateway/server/config.ts
@@ -3,4 +3,4 @@ export const DEFAULT_HOST =
 	process.env.SKYTH_GATEWAY_HOST ??
 	process.env.CLAUDE_GATEWAY_HOST ??
 	"skyth-gateway.local";
-export const PORT = Number(process.env.SKYTH_GATEWAY_PORT ?? 22000);
+export const PORT = Number(process.env.SKYTH_GATEWAY_PORT ?? 52000);
